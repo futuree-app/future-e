4a040a6dd92979e01e56a849ce7687f08cd52352
Quentin Brache <bonjourfuture@gmail.com>
2026-05-18 23:58:00 +0200

    Block search indexing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,14 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "futur•e",
   description: "Projection climatique personnelle",
+  robots: {
+    index: false,
+    follow: false,
+    googleBot: {
+      index: false,
+      follow: false,
+    },
+  },
 };
 
 export default function RootLayout({
